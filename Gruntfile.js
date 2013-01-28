bcb30395a2c400e7a52bd5f8cb5239b658443ee4
Jamie Stackhouse <jstackhouse@Jamies-MacBook-Pro.local>
2013-01-27 22:35:43 -0400

    Finished Gruntfile setup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,33 +4,54 @@ module.exports = function(grunt) {
 	//Project Configuration
 	grunt.initConfig({
 
-		/*less: {
+		less: {
 			development: {
 				options: {
 					paths: "less/include"
 				},
-				files: [
-					{src: "!less/include/*.less"},
-					{src: "less/**/
-					/*.less", dest: "public/stylesheets/*.css"}
-				]
+                files: [
+                    {
+                        expand: true,
+                        //flatten: true, //will make dest folder flat
+                        cwd: 'less/',
+                        src: ['**/*.less', '!include/*.less'],
+                        dest: 'public/stylesheets/',
+                        ext: '.css'
+                    }
+                ]
 			}
-		},*/
+		},
 
-		simplemocha: {
-			all: {
-				src: 'test/**/*.spec.js',
-				options: {
-					timeout: 5000,
-					ignoreLeaks: false,
-					ui: 'bdd',
-					reporter: 'spec'
-				}
-			}
-		}
+        watch: {
+            less: {
+                files: 'less/**/*.less',
+                tasks: ['less:development'],
+                options: {
+                    interrupt : true
+                }
+            },
 
+            scripts: {
+                files: ['**/*.js', '!node_modules/**/.js'],
+                tasks: ['exec:scriptTest', 'exec:test'],
+                options: {
+                    interrupt: true
+                }
+            }
+        },
+
+        exec: {
+            test: {
+                cmd: "mocha --colors test" 
+            },
+            scriptTest: {
+                cmd: 'clear && echo watch js'
+            }
+        }
 	});
-	grunt.loadNpmTasks('grunt-simple-mocha');
-	grunt.registerTask('test', ['simplemocha']);
+	grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('test', ['exec:test']);
 	grunt.registerTask('development', 'less:development');
 }
